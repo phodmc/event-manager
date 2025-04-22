4834985bc35377a17aeab8b4d425c223af6df7a5
Pho <pho.dmc5@gmail.com>
2025-04-22 22:29:30 +0000

    feat: initial commit

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello from the backend!");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
