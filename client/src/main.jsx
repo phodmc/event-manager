7e02002c0335271375af410df1c3725c7c0cf896
Pho <pho.dmc5@gmail.com>
2025-04-28 21:51:45 +0000

    feat: add react-router package and rename SignUp.js to SignUp.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,6 +6,7 @@ import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
 import SignInSide from "./SignInSide";
+import SignUp from "./SignUp";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
