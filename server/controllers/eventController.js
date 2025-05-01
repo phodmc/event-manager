68b14db42913509b0422a99eede57bd467d5e2cd
Pho <pho.dmc5@gmail.com>
2025-05-01 21:15:18 +0000

    feat: create controller functions for basic crud operations

---
+++ server/controllers/eventController.js
@@ -0,0 +1,54 @@
+const Event = require("../models/Event");
+
+const getAllEvents = async (req, res) => {
+  try {
+    const events = await Event.find();
+    res.json(events);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const createEvent = async (req, res) => {
+  const newEvent = new Event(req.body);
+  try {
+    const savedEvent = await newEvent.save();
+    res.status(201).json(savedEvent);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+const getEventById = async (req, res) => {
+  try {
+    const event = await Event.findById(req.params.id);
+
+    if (!event) res.status(400).json({ message: "Event not found" });
+    res.json(event);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const updateEvent = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const updatedEvent = await Event.findByIdAndUpdate(id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!updatedEvent) res.status(404).json({ message: "Event not found" });
+
+    res.json(updateEvent);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+module.exports = {
+  getAllEvents,
+  createEvent,
+  getEventById,
+  updateEvent,
+};
