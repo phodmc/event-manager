b0df214e906181a46f722292ac590e82826814ee
Pho <pho.dmc5@gmail.com>
2025-05-01 21:12:35 +0000

    feat: rename entry file from index.js to server.js

---
+++ server/server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+// routes
+const eventRoutes = require("./routes/eventRoutes");
+const userRoutes = require("./routes/userRoutes");
+
+// load environment variables
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+// connect to MongoDB Atlas
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("connected to MongoDB Atlas"))
+  .catch((err) => console.log("MongoDB connection error: ", err));
+
+// routes - for testing
+app.use("/", eventRoutes);
+
+// app.use("/", userRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
