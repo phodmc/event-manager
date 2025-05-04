1be2691eb670573a12ce546b37fb1885e6154459
Pho <pho.dmc5@gmail.com>
2025-05-04 22:26:52 +0000

    feat: add delete controller function

--- server/controllers/eventController.js
+++ server/controllers/eventController.js
@@ -20,10 +20,11 @@ const createEvent = async (req, res) => {
 };
 
 const getEventById = async (req, res) => {
+  const { id } = req.params;
   try {
-    const event = await Event.findById(req.params.id);
+    const event = await Event.findById(id);
 
-    if (!event) res.status(400).json({ message: "Event not found" });
+    if (!event) res.status(404).json({ message: "Event not found" });
     res.json(event);
   } catch (error) {
     res.status(500).json({ message: error.message });
@@ -46,9 +47,24 @@ const updateEvent = async (req, res) => {
   }
 };
 
+const deleteEvent = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const deletedEvent = await Event.findByIdAndDelete(id);
+
+    if (!deletedEvent) res.status(404).json({ message: "Event not found" });
+
+    res.status(204).send();
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
 module.exports = {
   getAllEvents,
   createEvent,
   getEventById,
   updateEvent,
+  deleteEvent,
 };
