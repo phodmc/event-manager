0885db72327baf914650fffd0834014a5aefa7c4
Pho <pho.dmc5@gmail.com>
2025-05-05 19:24:21 +0000

    feat: create auth middleware for route protection

--- server/routes/eventRoutes.js
+++ server/routes/eventRoutes.js
@@ -1,21 +1,22 @@
 const express = require("express");
 const router = express.Router();
 const eventController = require("../controllers/eventController");
+const authMiddleware = require("../middleware/authMiddleware");
 
 // router for fetching all events
 router.get("/", eventController.getAllEvents);
 
 // router for creating a new event
-router.post("/", eventController.createEvent);
+router.post("/", authMiddleware, eventController.createEvent);
 
 // router for fetching a single event by ID
 router.get("/:id", eventController.getEventById);
 
 // router for updating an event by ID
-router.put("/:id", eventController.updateEvent);
+router.put("/:id", authMiddleware, eventController.updateEvent);
 
 // router for deleting an event by ID
-router.delete("/:id", eventController.deleteEvent);
+router.delete("/:id", authMiddleware, eventController.deleteEvent);
 
 // router for fetching all events by user ID
 // router.get("/users/:userId/events", eventController.getEventsByUserId);
