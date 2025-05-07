50fe3166fc9bdc1ecbfce806c7a62fc9e361faf5
Pho <pho.dmc5@gmail.com>
2025-05-07 21:52:52 +0000

    feat: protect getting user routes with auth middleware

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
+const authMiddleware = require("../middleware/authMiddleware");
 
 // route to get all users
-router.get("/", userController.getAllUsers);
+router.get("/", authMiddleware, userController.getAllUsers);
 
 // route to get user by Id
-router.get("/:id", userController.getUserById);
+router.get("/:id", authMiddleware, userController.getUserById);
 
 // route to register a new user
 router.post("/register", userController.registerUser);
